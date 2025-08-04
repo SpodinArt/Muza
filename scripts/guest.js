6f54e5ccce241d6a4ef94661a8d02371ada838de
looker n a <gera.nikitin1996@gmail.com>
2025-08-04 14:53:52 +0300

    добавил файл скрипта guest.js замедления видео 0,5 подключил к guest.html

---
+++ scripts/guest.js
@@ -0,0 +1,6 @@
+
+        // Получаем объект видео по ID
+        const video = document.getElementById("bg-video");
+        
+        // Устанавливаем скорость воспроизведения на 0.5 (замедление в 2 раза)
+        video.playbackRate = 0.5;
