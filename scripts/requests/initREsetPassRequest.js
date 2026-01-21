798e7238e6aa687a31d2db37dc435dc744b5c8ca
looker n a <gera.nikitin1996@gmail.com>
2026-01-21 15:41:04 +0300

    инициализация запроса на смену пароля

---
+++ scripts/requests/initREsetPassRequest.js
@@ -0,0 +1,9 @@
+export function initREsetPassRequest(name, email, password, phone) {
+ let aaa = 89887453505
+  const resetpass_json = {
+    login: name,
+    email: email,
+    password: password,
+    phone_number: aaa,
+    //phone_number: phone,
+  };
